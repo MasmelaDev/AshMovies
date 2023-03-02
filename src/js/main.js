2e8b71911d6add9f8ae00bacb903dbf813022f9a
MasmelaDev <miguelmasmeladev@gmail.com>
2023-03-02 10:34:24 -0500

    added infinite scroll for genre section and search section

--- src/js/main.js
+++ src/js/main.js
@@ -10,33 +10,62 @@ const api = axios.create({
 
 const baseUrlImage = "https://image.tmdb.org/t/p";
 
-
-function callback(entries, observer){
+function callback(entries, observer) {
   entries
-  .filter(entry => entry.isIntersecting)
-  .forEach(entry=> {
-    if(entry.target.nodeName == "IMG"){
-      const parent = entry.target.parentNode
-      parent.classList.add("show")
-      const img = entry.target
-      const src = img.dataset.src
-      img.setAttribute("src",src)
+    .filter((entry) => entry.isIntersecting)
+    .forEach((entry) => {
+      if (entry.target.nodeName == "IMG") {
+        const parent = entry.target.parentNode;
+        parent.classList.add("show");
+        const img = entry.target;
+        const src = img.dataset.src;
+        img.setAttribute("src", src);
 
-
-      observer.unobserve(entry.target)
-    }
-    if (entry.target.nodeName == "ARTICLE"){
-      const article = entry.target
-      const src = article.dataset.src
-      article.style.backgroundImage = src
-      observer.unobserve(entry.target)
-    }
-
-  })
+        observer.unobserve(entry.target);
+      }
+      if (entry.target.nodeName == "ARTICLE") {
+        const article = entry.target;
+        const src = article.dataset.src;
+        article.style.backgroundImage = src;
+        observer.unobserve(entry.target);
+      }
+    });
 }
 
-const observer = new IntersectionObserver(callback,{})
+const observer = new IntersectionObserver(callback, {});
 
+let page = 1;
+
+const footerObserverInfiniteScroll = new IntersectionObserver((entries) => {
+  entries
+    .filter((entry) => entry.isIntersecting)
+    .forEach((entry) => {
+      console.log("a");
+      if (location.hash.startsWith("#genre=")) {
+        const genre = location.hash.split("=");
+        page++;
+        getAndAppendMovies(
+          ".genres__resultMoviesContainer",
+          "/discover/movie",
+          { params: { with_genres: genre[1], page } },
+          false
+        );
+      }
+      if (location.hash.startsWith("#search=")) {
+        const [_, query] = location.hash.split("=");
+        queryDecode = decodeURI(query);
+        page++;
+        getAndAppendMovies(
+          ".search__resultMoviesContainer",
+          "/search/movie",
+          { params: { query: queryDecode, page } },
+          false
+        );
+      }
+    });
+});
+
+const footer = document.querySelector("footer");
 
 async function getTopRatedtMovies() {
   const { data } = await api(`/movie/top_rated`);
@@ -47,7 +76,7 @@ async function getTopRatedtMovies() {
   const random = Math.floor(Math.random() * 20);
   img.src = `${baseUrlImage}/original${data.results[random].backdrop_path}`;
   img.alt = "top rated movie image";
-  img.className = "hero__img"
+  img.className = "hero__img";
   const overview = document.querySelector(".hero__overview");
   overview.textContent = data.results[random].overview;
   const rate = document.querySelector("#rate");
@@ -66,8 +95,8 @@ function getGenresPreview() {
       },
     });
     const movie = data.results[18];
-    genre.dataset.src =`url(${baseUrlImage}/w342${movie.poster_path})`
-    observer.observe(genre)
+    genre.dataset.src = `url(${baseUrlImage}/w342${movie.poster_path})`;
+    observer.observe(genre);
   });
 }
 
@@ -117,63 +146,65 @@ const carouselGenres = new Glider(document.querySelector(".genres__carousel"), {
   ],
 });
 
-
-
 async function getAndAppendMovies(
   parentContainer,
   path,
   optionalConfig,
+  clean = true
 ) {
   const moviesContainer = document.querySelector(parentContainer);
-  moviesContainer.innerHTML = "";
+  if (clean) {
+    moviesContainer.innerHTML = "";
+  }
   const moviesCards = [];
-  try{
-
+  try {
     const { data } = await api(path, optionalConfig);
-    
+
     const movies = data.results;
-    if(movies.length < 2){
-      console.log("aa")
-      const container = document.createElement("div")
-      container.style = "width:700px;height:400px;display:flex;align-items:center;justify-content:center;"
-      const text = document.createElement("h2")
-      container.append(text)
-      text.innerText="We're sorry, we couldn't find what you're looking for."
-      moviesContainer.append(container)
-      return false
+    if (movies.length < 2) {
+      footerObserverInfiniteScroll.unobserve(footer);
+      const container = document.createElement("div");
+      container.style =
+        "width:100%;height:600px;display:flex;align-items:center;justify-content:center;";
+      const text = document.createElement("h2");
+      container.append(text);
+      text.innerText = "We're sorry,but there's no matches for you search";
+      moviesContainer.append(container);
+      return false;
     }
     movies.forEach((movie) => {
-      if(movie.poster_path){
+      if (movie.poster_path && !movie.adult) {
         const movieCard = document.createElement("article");
         movieCard.classList.add("movieCard");
-        movieCard.id = movie.id
+        movieCard.id = movie.id;
         const movieImg = document.createElement("img");
         movieImg.classList.add("movieImg");
         movieImg.alt = movie.title;
         movieImg.dataset.src = baseUrlImage + "/w342" + movie.poster_path;
-        observer.observe(movieImg)
+        observer.observe(movieImg);
 
         const movieTitle = document.createElement("h2");
         movieTitle.textContent = movie.title;
         movieTitle.classList.add("movieTitle");
-        
+
         movieCard.append(movieImg, movieTitle);
-        
+
         moviesCards.push(movieCard);
+      }
+    });
+    console.log(data);
+    moviesContainer.append(...moviesCards);
+
+    return true;
+  } catch (err) {
+    console.log(err);
+    const container = document.createElement("div");
+    const text = document.createElement("h2");
+    container.append(text);
+    text.innerText = "We're sorry, we couldn't find what you're looking for.";
+    moviesContainer.append(container);
   }
-});
-console.log(data);
-moviesContainer.append(...moviesCards);
-return true
-}catch{
-  const container = document.createElement("div")
-  const text = document.createElement("h2")
-  container.append(text)
-  text.innerText="We're sorry, we couldn't find what you're looking for."
-  moviesContainer.append(container)
-}
 }
 
 getGenresPreview();
 getTopRatedtMovies();
-
--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,212 +1,155 @@
 const genresContainer = document.querySelector(".genres__carousel");
 genresContainer.addEventListener("click", (entry) => {
-  if (entry.target.nodeName === "ARTICLE" ) {
+  if (entry.target.nodeName === "ARTICLE") {
     const genre = entry.target;
-    location.hash = `genre=${genre.id.slice(2)}=${genre.innerText}`;  }
-  if (entry.target.nodeName === "H2" ) {
-      const genre = entry.target.parentNode;
-      location.hash = `genre=${genre.id.slice(2)}=${genre.innerText}`;  }
+    location.hash = `genre=${genre.id.slice(2)}=${genre.innerText}`;
+  }
+  if (entry.target.nodeName === "H2") {
+    const genre = entry.target.parentNode;
+    location.hash = `genre=${genre.id.slice(2)}=${genre.innerText}`;
+  }
 });
 
+const changeSection__ul = document.querySelector(".changeSection__ul");
+changeSection__ul.addEventListener("click", (entry) => {
+  if (entry.target.nodeName === "LI") {
+    const li = entry.target;
 
-const changeSection__ul = document.querySelector(".changeSection__ul")
-changeSection__ul.addEventListener("click",(entry)=>{
-  if (entry.target.nodeName === "LI"){
-    const li = entry.target
-
-
-    location.hash = li.innerText
-    
+    location.hash = li.innerText;
   }
-})
+});
 
-const searchIcon = document.querySelector(".searchIcon")
+const searchIcon = document.querySelector(".searchIcon");
 
-searchIcon.addEventListener("click" ,() => {
-  location.hash = "search="+searchInput.value
-})
+searchIcon.addEventListener("click", () => {
+  location.hash = "search=" + searchInput.value;
+});
 
+const main = document.querySelector(".main");
 
-const main = document.querySelector(".main")
-
-main.addEventListener("click",(entry)=>{
-  if(entry.target.className =="movieImg"){
-    location.hash = "movie="+entry.target.parentNode.id
-  }
-})
-
+main.addEventListener("click", showMovie);
 
 window.addEventListener("hashchange", navigator, false);
 window.addEventListener("load", navigator, false);
 
 function navigator() {
-  if(location.hash.startsWith("#Trending")){
-    changeSection("Trending","/trending/movie/day",{ params: { region: "US", page: 3 } })
-  }
-  else if(location.hash.startsWith("#New")){
-    changeSection("New","/movie/now_playing",{ params: { region: "US"} })
-  }
-  else if(location.hash.startsWith("#Popular")){
-    changeSection("Popular","/movie/popular",{ params: { region: "US", page: 3 } })
-  }
-  else if (location.hash.startsWith("#genre=")) {
+  if (location.hash.startsWith("#Trending")) {
+    changeSection("Trending", "/trending/movie/day", {
+      params: { region: "US", page: 3 },
+    });
+  } else if (location.hash.startsWith("#New")) {
+    changeSection("New", "/movie/now_playing", { params: { region: "US" } });
+  } else if (location.hash.startsWith("#Popular")) {
+    changeSection("Popular", "/movie/popular", {
+      params: { region: "US", page: 3 },
+    });
+  } else if (location.hash.startsWith("#genre=")) {
     showGenre();
   } else if (location.hash.startsWith("#back")) {
-    backToGenres()
+    back();
   } else if (location.hash.startsWith("#search=")) {
-    showSearch()
-  } else if (location.hash.startsWith("#movie=")) {
-    showMovie()
+    showSearch();
   } 
 }
 
-
 function showGenre() {
+  const genres__resultContainer = document.querySelector(
+    ".genres__resultContainer"
+  );
+  const explore__section = [...document.querySelectorAll(".explore__section")];
+  genres__resultContainer.innerHTML = "";
+  explore__section.forEach((section) => section.classList.remove("active"));
+  const genre = location.hash.split("=");
 
-const genres__resultContainer = document.querySelector(".genres__resultContainer")
-  const explore__section  = [...document.querySelectorAll(".explore__section")];
+  const genres__resultContainerTitle = document.createElement("h2");
+  genres__resultContainerTitle.className = "genres__resultContainerTitle";
+  genres__resultContainerTitle.innerText = decodeURI(genre[2]);
+  genres__resultContainerTitle.addEventListener("click", () => {
+    location.hash = "back";
+  });
 
-  explore__section.forEach(section => section.classList.remove("active"))
-  const genre = location.hash.split("=")
+  const genres__resultMoviesContainer = document.createElement("div");
+  genres__resultMoviesContainer.className = "genres__resultMoviesContainer";
 
-  const genres__resultContainerTitle = document.createElement("h2")
-  genres__resultContainerTitle.className = "genres__resultContainerTitle"
-  genres__resultContainerTitle.innerText = decodeURI(genre[2]) 
-  genres__resultContainerTitle.addEventListener("click",()=>{
-    location.hash = "back"
-  })
-
-  const genres__resultMoviesContainer = document.createElement("div")
-  genres__resultMoviesContainer.className = "genres__resultMoviesContainer"
-
-
-
-  const leftRow =document.createElement("img")
-  leftRow.src = "./src/assets/angle-left-solid.svg"
-  leftRow.alt= "leftRow"
-  leftRow.className = "leftRow"
-  const prevButton = document.createElement ("button")
-  prevButton.className = "leftRowContainerPage"
-  prevButton.append(leftRow)
-
-  const counterContainer = document.createElement("div")
-  const counter = document.createElement("span")
-  counter.innerText = "1"
-  counter.id = "counter"
-  counterContainer.appendChild(counter)
-
-  const rightRow =document.createElement("img")
-  rightRow.src = "./src/assets/angle-right-solid.svg"
-  rightRow.alt= "rightRow"
-  rightRow.className = "rightRow"
-  const nextButton = document.createElement ("button")
-  nextButton.className = "rightRowContainerPage"
-  nextButton.append(rightRow)
-
-  const genres__resultControls = document.createElement("div")
-  genres__resultControls.className = "genres__resultControls"
-  genres__resultControls.append(prevButton,counterContainer,nextButton)
-
-  genres__resultControls.addEventListener("click",(entry)=>{
-    if (entry.target.classList.contains("rightRow")){
-
-      counter.innerText = parseInt(counter.innerText)+1
-      getAndAppendMovies(".genres__resultMoviesContainer","/discover/movie",{ params: { with_genres: genre[1], page:counter.innerText} })
-    }
-    if(entry.target.classList.contains("leftRow") && parseInt(counter.innerText) > 1){
-      counter.innerText = parseInt(counter.innerText)-1
-      getAndAppendMovies(".genres__resultMoviesContainer","/discover/movie",{ params: { with_genres: genre[1], page:counter.innerText} })
-    }
-  })
-
-  genres__resultContainer.append(genres__resultContainerTitle,genres__resultMoviesContainer,genres__resultControls)
-
-
-  getAndAppendMovies(".genres__resultMoviesContainer",
-  "/discover/movie",
-  { params: { with_genres: genre[1]} }
+  genres__resultContainer.append(
+    genres__resultContainerTitle,
+    genres__resultMoviesContainer
   );
 
-  genres__resultContainer.classList.add("active")
-  genres__resultMoviesContainer.classList.add("active")
-  // setTimeout(()=>{genres__resultContainer.classList.add("active");},200)
+  getAndAppendMovies(".genres__resultMoviesContainer", "/discover/movie", {
+    params: { with_genres: genre[1] },
+  });
+
+  genres__resultContainer.classList.add("active");
+  genres__resultMoviesContainer.classList.add("active");
+  footerObserverInfiniteScroll.observe(footer);
+
 }
 
-function backToGenres() {
-  const genres = document.querySelector(".genres")
-  const genres__resultContainer = document.querySelector(".genres__resultContainer")
-  const search = document.querySelector(".search ")
-  setTimeout(()=>{
-    genres__resultContainer.classList.remove("active")
-    search.classList.remove("active")
-  },500)
-
-  genres.classList.add("active")
-  genres__resultContainer.innerHTML=""
-  search.innerHTML = ""
-}
-
-async function changeSection(section,path,optionalConfig={}){
-
-  
-  const currentSection = document.querySelector(".currentSection")
-
-  currentSection.innerHTML=""
-  currentSection.id=section
-
-  const changeSection__li = [...document.querySelectorAll(".changeSection__li")]
-  changeSection__li.forEach(li =>{
-    li.classList.remove("active")
-  })
-  const liActive = changeSection__li.find(li => li.innerText === section)
-  liActive.classList.add("active")
-
-
-
-
-
-  const leftRow =document.createElement("img")
-  leftRow.src = "./src/assets/angle-left-solid.svg"
-  leftRow.alt= "leftRow"
-  leftRow.className = "leftRow"
-  const prevButton = document.createElement ("button")
-  prevButton.className = "leftRowContainer prevButton"
-  prevButton.append(leftRow)
-
-  const moviesContainer = document.createElement("div")
-  moviesContainer.className ="moviesContainer"
-
-  const rightRow =document.createElement("img")
-  rightRow.src = "./src/assets/angle-right-solid.svg"
-  rightRow.alt= "rightRow"
-  rightRow.className = "rightRow"
-  const nextButton = document.createElement ("button")
-  nextButton.className = "rightRowContainer nextButton"
-  nextButton.append(rightRow)
-
-  const controls = document.createElement("div")
-  controls.className = "controls"
-  
-
-  const currentSection__container = document.createElement("div")
-  currentSection__container.className = "currentSection__container"
-  currentSection__container.append(prevButton,moviesContainer,nextButton)
-
-
-  currentSection.append(currentSection__container,controls)
-  
-  
-
-
-
-  await getAndAppendMovies(
-    ".moviesContainer",
-    path,
-    optionalConfig,
-   
+function back() {
+  page = 1;
+  footerObserverInfiniteScroll.unobserve(footer);
+  const genres = document.querySelector(".genres");
+  const genres__resultContainer = document.querySelector(
+    ".genres__resultContainer"
   );
-  
-   const moviesCarousel = await new Glider(
+  const search = document.querySelector(".search ");
+  setTimeout(() => {
+    genres__resultContainer.classList.remove("active");
+    search.classList.remove("active");
+  }, 500);
+
+  genres.classList.add("active");
+  genres__resultContainer.innerHTML = "";
+  search.innerHTML = "";
+}
+
+async function changeSection(section, path, optionalConfig = {}) {
+  const currentSection = document.querySelector(".currentSection");
+
+  currentSection.innerHTML = "";
+  currentSection.id = section;
+
+  const changeSection__li = [
+    ...document.querySelectorAll(".changeSection__li"),
+  ];
+  changeSection__li.forEach((li) => {
+    li.classList.remove("active");
+  });
+  const liActive = changeSection__li.find((li) => li.innerText === section);
+  liActive.classList.add("active");
+
+  const leftRow = document.createElement("img");
+  leftRow.src = "./src/assets/angle-left-solid.svg";
+  leftRow.alt = "leftRow";
+  leftRow.className = "leftRow";
+  const prevButton = document.createElement("button");
+  prevButton.className = "leftRowContainer prevButton";
+  prevButton.append(leftRow);
+
+  const moviesContainer = document.createElement("div");
+  moviesContainer.className = "moviesContainer";
+
+  const rightRow = document.createElement("img");
+  rightRow.src = "./src/assets/angle-right-solid.svg";
+  rightRow.alt = "rightRow";
+  rightRow.className = "rightRow";
+  const nextButton = document.createElement("button");
+  nextButton.className = "rightRowContainer nextButton";
+  nextButton.append(rightRow);
+
+  const controls = document.createElement("div");
+  controls.className = "controls";
+
+  const currentSection__container = document.createElement("div");
+  currentSection__container.className = "currentSection__container";
+  currentSection__container.append(prevButton, moviesContainer, nextButton);
+
+  currentSection.append(currentSection__container, controls);
+
+  await getAndAppendMovies(".moviesContainer", path, optionalConfig);
+
+  const moviesCarousel = await new Glider(
     document.querySelector(".moviesContainer"),
     {
       slidesToShow: 2,
@@ -242,128 +185,107 @@ async function changeSection(section,path,optionalConfig={}){
             slidesToShow: 5,
             slidesToScroll: 5,
           },
-        },   {
+        },
+        {
           // screens greater than >= 1320px
           breakpoint: 1320,
           settings: {
             slidesToShow: 6,
             slidesToScroll: 6,
           },
-        }
+        },
       ],
     }
-  ); 
-
-  currentSection__container.classList.add("active")
-}
-
-
-function showSearch(){
-  const search = document.querySelector(".search")
-  const explore__section  = [...document.querySelectorAll(".explore__section")];
-  search.innerHTML = ""
-  explore__section.forEach(section => section.classList.remove("active"))
-
-  const [_,query] = location.hash.split("=")
-  queryDecode = decodeURI(query)
-  const search__resultContainerTitle = document.createElement("h2")
-  search__resultContainerTitle.className = "search__resultContainerTitle"
-  search__resultContainerTitle.innerText = queryDecode
-  search__resultContainerTitle.addEventListener("click",()=>{
-    location.hash = "back"
-  })
-
-  const search__resultMoviesContainer = document.createElement("div")
-  search__resultMoviesContainer.className = "search__resultMoviesContainer"
-
-
-
-  const leftRow =document.createElement("img")
-  leftRow.src = "./src/assets/angle-left-solid.svg"
-  leftRow.alt= "leftRow"
-  leftRow.className = "leftRow"
-  const prevButton = document.createElement ("button")
-  prevButton.className = "leftRowContainerPage"
-  prevButton.append(leftRow)
-
-  const counterContainer = document.createElement("div")
-  const counter = document.createElement("span")
-  counter.innerText = "1"
-  counter.id = "counter"
-  counterContainer.appendChild(counter)
-
-  const rightRow =document.createElement("img")
-  rightRow.src = "./src/assets/angle-right-solid.svg"
-  rightRow.alt= "rightRow"
-  rightRow.className = "rightRow"
-  const nextButton = document.createElement ("button")
-  nextButton.className = "rightRowContainerPage"
-  nextButton.append(rightRow)
-
-  const search__resultControls = document.createElement("div")
-  search__resultControls.className = "search__resultControls"
-  search__resultControls.append(prevButton,counterContainer,nextButton)
-
-  search__resultControls.addEventListener("click",(entry)=>{
-
-
-    if (entry.target.classList.contains("rightRow")){
-
-      counter.innerText = parseInt(counter.innerText)+1
-      getAndAppendMovies(".search__resultMoviesContainer","/search/movie",{ params: {query:queryDecode,page:counter.innerText} })
-    }
-    if(entry.target.classList.contains("leftRow") && parseInt(counter.innerText) > 1){
-      counter.innerText = parseInt(counter.innerText)-1
-      if(getAndAppendMovies(".search__resultMoviesContainer","/search/movie",{ params: {query:queryDecode,page:counter.innerText} })){}
-    }
-  })
-
-  search.append(search__resultContainerTitle,search__resultMoviesContainer,search__resultControls)
-
-
-  getAndAppendMovies(".search__resultMoviesContainer",
-  "/search/movie",
-  { params: {query:queryDecode} }
   );
 
-  search.classList.add("active")
-  search__resultMoviesContainer.classList.add("active")
-  searchInput.value = ""
+  currentSection__container.classList.add("active");
 }
 
+function showSearch() {
+  const genres__resultContainer = document.querySelector(
+    ".genres__resultContainer"
+  );
+  genres__resultContainer.innerHTML = "";
+  page = 1;
+  const search = document.querySelector(".search");
+  const explore__section = [...document.querySelectorAll(".explore__section")];
+  search.innerHTML = "";
+  explore__section.forEach((section) => section.classList.remove("active"));
 
-async function showMovie(){
-  const body = document.querySelector("body")
-  body.classList.add("inactive")
-  const [_,id] = location.hash.split("=")
-  const {data} = await api(`/movie/${id}`)
-  console.log(data)
-  const movieDescription = document.querySelector(".movieDescription")
-  movieDescription.classList.add("active")
-  const movieDescription__imgContainer = document.querySelector(".movieDescription__imgContainer")
-  const movieDescriptionImg = document.createElement("img")
-  movieDescriptionImg.src = `${baseUrlImage}/original${data.backdrop_path}`
-  movieDescriptionImg.alt="movieImg"
-  movieDescriptionImg.className = "movieDescription__img"
-  movieDescription__imgContainer.appendChild(movieDescriptionImg)
-  const movieDescription__title = document.querySelector(".movieDescription__title")
-  movieDescription__title.innerText = data.title
-  const movieDescription__overview = document.querySelector(".movieDescription__overview")
-  movieDescription__overview.innerText = data.overview
-  movieRate.innerText = Math.round(data.vote_average)
+  const [_, query] = location.hash.split("=");
+  queryDecode = decodeURI(query);
+  const search__resultContainerTitle = document.createElement("h2");
+  search__resultContainerTitle.className = "search__resultContainerTitle";
+  search__resultContainerTitle.innerText = queryDecode;
+  search__resultContainerTitle.addEventListener("click", () => {
+    location.hash = "back";
+  });
 
+  const search__resultMoviesContainer = document.createElement("div");
+  search__resultMoviesContainer.className = "search__resultMoviesContainer";
+
+  search.append(search__resultContainerTitle, search__resultMoviesContainer);
+
+  getAndAppendMovies(".search__resultMoviesContainer", "/search/movie", {
+    params: { query: queryDecode },
+  });
+
+  search.classList.add("active");
+  search__resultMoviesContainer.classList.add("active");
+  searchInput.value = "";
+  footerObserverInfiniteScroll.observe(footer);
 }
 
-const movieDescription__back = document.querySelector(".movieDescription__back")
-movieDescription__back.addEventListener("click",(entry)=>{
-  location.hash = "home"
-  const body = document.querySelector("body")
-  body.classList.remove("inactive")
-  const movieDescription = document.querySelector(".movieDescription")
-  movieDescription.classList.remove("active")
+async function showMovie(entry) {
+
+  if (entry.target.className == "movieImg") {
+    const body = document.querySelector("body");
+    body.classList.add("inactive");
+    const id = entry.target.parentNode.id;
+    const { data } = await api(`/movie/${id}`);
+    console.log(data);
+    const movieDescription = document.querySelector(".movieDescription");
+    movieDescription.classList.add("active");
+    const movieDescription__imgContainer = document.querySelector(
+      ".movieDescription__imgContainer"
+    );
+    const movieDescriptionImg = document.createElement("img");
+    movieDescriptionImg.src = `${baseUrlImage}/original${data.backdrop_path}`;
+    movieDescriptionImg.alt = "movieImg";
+    movieDescriptionImg.className = "movieDescription__img";
+    movieDescription__imgContainer.appendChild(movieDescriptionImg);
+    const movieDescription__title = document.querySelector(
+      ".movieDescription__title"
+    );
+    movieDescription__title.innerText = data.title;
+    const movieDescription__overview = document.querySelector(
+      ".movieDescription__overview"
+    );
+    movieDescription__overview.innerText = data.overview;
+    movieRate.innerText = Math.round(data.vote_average);
+   
+
+  
+  }
+}
+
+const movieDescription__back = document.querySelector(
+  ".movieDescription__back"
+);
+
+movieDescription__back.addEventListener("click", (entry) => {
+  const body = document.querySelector("body");
+  body.classList.remove("inactive");
+  const movieDescription = document.querySelector(".movieDescription");
+
+  movieDescription.classList.remove("active");
   const movieDescription__imgContainer = document.querySelector(".movieDescription__imgContainer")
-  movieDescription__imgContainer.innerHTML = ""
-})
 
-if(location.hash != "#New" && location.hash != "#Popular" && location.hash != "#Trending") changeSection("Trending","/trending/movie/day")
+  movieDescription__imgContainer.innerHTML = "";
+});
 
+if (
+  location.hash != "#New" &&
+  location.hash != "#Popular" &&
+  location.hash != "#Trending"
+){changeSection("Trending", "/trending/movie/day");}
